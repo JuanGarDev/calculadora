b6cb034a7b00a1ccb75c65d7da9be8ea8c89b99f
Juan GarciaPC <fnxbeats@outlook.com>
2023-03-19 19:19:15 -0600

    Calculadora funcional

---
+++ src/main/java/test/testeando.java
@@ -0,0 +1,15 @@
+
+
+package test;
+
+import domain.Operaciones;
+
+public class testeando{
+    public static void main(String[] args) {
+        
+
+        Operaciones operacion = new Operaciones();
+        
+        System.out.println(operacion.sumar(17, 27));
+    }
+}
